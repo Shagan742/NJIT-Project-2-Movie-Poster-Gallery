adb28d3d0d0bebf41e61382c8b72988a1ace299a
Shagan742 <526ssangha@frhsd.com>
2025-12-09 14:49:57 -0500

    now finished all website

--- site.js
+++ site.js
@@ -33,7 +33,7 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         siteTitle: "Shagan's Top 8 Movies for Kids",
+         siteTitle: "IMDB + Shagan's Top 8 Movies",
          owner: "Shagan Sangha",
          repositoryLink: "https://github.com/Shagan742/NJIT-Project-2-Movie-Poster-Gallery"
          
